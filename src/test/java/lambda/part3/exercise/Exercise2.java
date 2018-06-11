75a71d8cffd7b467a25f6ba1f61551991850c25f
thewalkingwind <tehwlaknigwnid@gmail.com>
2018-06-11 20:37:53 +0300

    Exc2

--- src/test/java/lambda/part3/exercise/Exercise2.java
+++ src/test/java/lambda/part3/exercise/Exercise2.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
